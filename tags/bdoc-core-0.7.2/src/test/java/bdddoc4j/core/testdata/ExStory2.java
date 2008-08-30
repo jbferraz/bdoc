15212ccd8bce41cc6fef9ce697f598931f974404
perottobc <perottobc@bf520d50-e954-0410-874e-2b0b529f9ca0>
2008-08-30 12:57:15 +0000

    [maven-release-plugin]  copy for tag bdoc-core-0.7.2

---
+++ tags/bdoc-core-0.7.2/src/test/java/bdddoc4j/core/testdata/ExStory2.java
@@ -0,0 +1,52 @@
+/**
+ * The MIT License
+ * 
+ * Copyright (c) 2008 Per Otto Bergum Christensen
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package bdddoc4j.core.testdata;
+
+/**
+ * Example of a story reference not implementing the interface
+ * UserStoryDescription, just the methods id(), name() and narrative().
+ * 
+ * @author Per Otto Bergum Christensen
+ */
+public enum ExStory2 {
+
+	TEST_STORY(1, "As a", "I want", "so that");
+
+	private Integer id;
+	private String[] narrative;
+
+	ExStory2(Integer id, String... narrative) {
+		this.id = id;
+		this.narrative = narrative;
+	}
+
+	public Integer id() {
+		return id;
+	}
+
+	public String[] narrative() {
+		return narrative;
+	}
+}

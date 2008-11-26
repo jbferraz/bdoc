049a7dcde0ec28769398705e08be605328cbfc23
perottobc <perottobc@bf520d50-e954-0410-874e-2b0b529f9ca0>
2008-11-26 20:34:47 +0000

    [maven-release-plugin]  copy for tag bdoc-core-0.7.7

---
+++ tags/bdoc-core-0.7.7/src/main/java/bdddoc4j/core/domain/Scenario.java
@@ -0,0 +1,91 @@
+/**
+ * The MIT License
+ * 
+ * Copyright (c) 2008 Per Otto Bergum Christensen
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package bdddoc4j.core.domain;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Locale;
+
+import org.apache.commons.lang.Validate;
+
+import bdddoc4j.core.util.CamelCaseToSentenceTranslator;
+import bdddoc4j.core.util.SentenceToLineSplit;
+
+/**
+ * @author Per Otto Bergum Christensen
+ */
+public class Scenario {
+
+	public enum Pattern {
+		NO(new Locale("no"), "gitt", "Naar", "Saa"), EN(Locale.ENGLISH, "given", "When", "Then");
+		private final String[] template;
+		private final Locale locale;
+
+		private Pattern(Locale locale, String... template) {
+			this.template = template;
+			this.locale = locale;
+		}
+
+		public Locale locale() {
+			return locale;
+		}
+
+		public static boolean match(String camelCaseSentence) {
+			return (null != find(camelCaseSentence));
+		}
+
+		public static Pattern find(String camelCaseSentence) {
+			for (Pattern pattern : Pattern.values()) {
+				if (camelCaseSentence.startsWith(pattern.template[0])) {
+					return pattern;
+				}
+			}
+			return null;
+		}
+	}
+
+	private final List<String> line = new ArrayList<String>();
+
+	public Scenario(String camelCaseSentence) {
+		super();
+
+		Pattern pattern = Pattern.find(camelCaseSentence);
+		Validate.notNull(pattern, "pattern not found for " + camelCaseSentence);
+
+		for (String ln : SentenceToLineSplit.split(camelCaseSentence, pattern.template[1], pattern.template[2])) {
+			line.add(CamelCaseToSentenceTranslator.translate(ln, pattern.locale()));
+		}
+	}
+
+	public List<String> getLines() {
+		return Collections.unmodifiableList(line);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		return (obj instanceof Scenario) && ((Scenario) obj).line.equals(line);
+	}
+}

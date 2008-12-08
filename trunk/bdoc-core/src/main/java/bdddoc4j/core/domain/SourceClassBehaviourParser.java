619536ca3b890ded60e2d9cd0bcf37494650e7d5
perottobc <perottobc@bf520d50-e954-0410-874e-2b0b529f9ca0>
2008-12-08 23:58:01 +0000

    Adding support for advanced scenario specification

---
+++ trunk/bdoc-core/src/main/java/bdddoc4j/core/domain/SourceClassBehaviourParser.java
@@ -0,0 +1,53 @@
+/**
+ * The MIT License
+ * 
+ * Copyright (c) 2008 Per Otto Bergum Christensen
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package bdddoc4j.core.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import bdddoc4j.core.util.JavaCodeUtil;
+
+public class SourceClassBehaviourParser {
+
+	private String javaClassSource;
+	private String[] testMethods;
+
+	public SourceClassBehaviourParser(String javaClassSource, String[] testMethods) {
+		this.javaClassSource = javaClassSource;
+		this.testMethods = testMethods;
+	}
+
+	public List<Scenario> getScenarios() {
+		List<Scenario> result = new ArrayList<Scenario>();
+		
+		for (String testMethod : testMethods) {			
+			String testMethodSource = JavaCodeUtil.javaBlockAfter(javaClassSource, testMethod );
+			result.add( new Scenario( JavaCodeUtil.getGivenWhenThenMethods(testMethodSource) ) );			
+		}
+		
+		return result;
+	}
+
+}
---
+++ trunk/bdoc-core/src/test/java/bdddoc4j/core/domain/TestSourceClassBehaviourParser.java
@@ -0,0 +1,55 @@
+/**
+ * The MIT License
+ * 
+ * Copyright (c) 2008 Per Otto Bergum Christensen
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package bdddoc4j.core.domain;
+
+import static junit.framework.Assert.assertTrue;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.junit.Ignore;
+import org.junit.Test;
+
+import bdddoc4j.core.doc.Ref;
+import bdddoc4j.core.doc.Story;
+
+@Ref( Story.ADVANCED_SCENARIO_SPECIFICATION  )
+public class TestSourceClassBehaviourParser {
+
+	@Test
+	public void shouldComposeScenarioFromTestConstructedWithGivenWhenThenMethods() throws IOException {
+		String stackJava = FileUtils.readFileToString(new File("src/test/java/integrationtestclasses/stack/Stack.java"));
+
+		String[] testMethods = new String[] { "shouldPopSecondPushedValueFirst" };
+		SourceClassBehaviourParser sourceClassBehaviourParser = new SourceClassBehaviourParser(stackJava, testMethods);
+
+		assertTrue(sourceClassBehaviourParser.getScenarios().contains(
+				"givenAStackWithTwoPushedValuesWhenPopIsCalledThenTheLastItemPushedShouldBeReturnedAndTheValueShouldNotRemainInTheStack"));
+	}
+	
+	//test at en metode uten et scenario ikke gir feil
+
+}

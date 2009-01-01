994be4652cdf40c87f728df8dfa72403a8dd1edd
micael.vesterlund <micael.vesterlund@bf520d50-e954-0410-874e-2b0b529f9ca0>
2009-01-01 22:26:18 +0000

    Fixed some build failures.

--- bdoc-core/src/test/java/com/googlecode/bdoc/clog/TestChangeLogBehaviour.java
+++ bdoc-core/src/test/java/com/googlecode/bdoc/clog/TestChangeLogBehaviour.java
@@ -1,3 +1,26 @@
+/**
+ * The MIT License
+ * 
+ * Copyright (c) 2008 Per Otto Bergum Christensen
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 package com.googlecode.bdoc.clog;
 
 import org.junit.Test;
@@ -11,7 +34,7 @@ import com.googlecode.bdoc.doc.testdata.RefClass;
 @Ref(Story.CHANGE_LOG)
 @RefClass(ChangeLog.class)
 public class TestChangeLogBehaviour {
-	
+
 	private ChangeLog givenAnNewChangeLog() {
 		return new ChangeLog();
 	}
@@ -26,17 +49,16 @@ public class TestChangeLogBehaviour {
 
 	private void thenEnsureTheChangeLogContainsTheBDoc() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	private void thenEnsureTheLatestDiffInTheChangeLogContainsTheSpecification() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	private void whenABDocWithOneSpecificationIsScannedByTheChangeLog(ChangeLog changeLog) {
-		BDoc bdoc = BDocTestHelper.bdocWithOneSpecification();		
+		BDoc bdoc = BDocTestHelper.bdocWithOneSpecification();
 	}
-
 
 }

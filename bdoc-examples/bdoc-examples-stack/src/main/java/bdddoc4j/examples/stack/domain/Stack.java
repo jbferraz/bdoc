26821353be06073917354673cfe702f2b4250cdb
perottobc <perottobc@bf520d50-e954-0410-874e-2b0b529f9ca0>
2009-01-28 19:50:30 +0000

    Updated author tag and license info

--- bdoc-examples/bdoc-examples-stack/src/main/java/bdddoc4j/examples/stack/domain/Stack.java
+++ bdoc-examples/bdoc-examples-stack/src/main/java/bdddoc4j/examples/stack/domain/Stack.java
@@ -1,7 +1,7 @@
 /**
  * The MIT License
  * 
- * Copyright (c) 2008 Per Otto Bergum Christensen
+ * Copyright (c) 2008, 2009 @Author(s)
  * 
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -26,7 +26,9 @@ package bdddoc4j.examples.stack.domain;
 
 import java.util.Vector;
 
-public class Stack<E> extends Vector<E> {
+/**
+ *  @author Per Otto Bergum Christensen
+ */public class Stack<E> extends Vector<E> {
 
 	private static final long serialVersionUID = 1L;
 
--- bdoc-examples/bdoc-examples-stack/src/test/java/bdddoc4j/examples/stack/domain/TestStack.java
+++ bdoc-examples/bdoc-examples-stack/src/test/java/bdddoc4j/examples/stack/domain/TestStack.java
@@ -1,7 +1,7 @@
 /**
  * The MIT License
  * 
- * Copyright (c) 2008 Per Otto Bergum Christensen
+ * Copyright (c) 2008, 2009 @Author(s)
  * 
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -35,6 +35,9 @@ import org.junit.Test;
 import bdddoc4j.examples.Ref;
 import bdddoc4j.examples.Story;
 
+/**
+ *  @author Per Otto Bergum Christensen
+ */
 @Ref(Story.STACK)
 public class TestStack {
 

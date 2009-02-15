76f2759b8d56bd6988c271f9bb03ead480a5c535
perottobc <perottobc@bf520d50-e954-0410-874e-2b0b529f9ca0>
2009-02-15 21:20:44 +0000

    Some refactoring of bdd-name and prep for difflog report

---
+++ trunk/bdoc-examples/bdoc-examples-stack/src/test/java/com/googlecode/bdoc/prototypes/TestAccountPrototypeBehaviour.java
@@ -0,0 +1,61 @@
+/**
+ * The MIT License
+ * 
+ * Copyright (c) 2008, 2009 @Author(s)
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package com.googlecode.bdoc.prototypes;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.googlecode.bdoc.examples.account.Account;
+
+public class TestAccountPrototypeBehaviour {
+
+	private Account account;
+
+	@Before
+	public void resetAccount() {
+		account = null;
+	}
+
+	@Test
+	public void should_update_balance_on_withdraw() {
+		given_an_initial_balance_of_$1(200);
+		when_I_withdraw_$1(20);
+		then_my_balance_should_be_$1(180);
+	}
+
+	private void given_an_initial_balance_of_$1(int balance) {
+		account = new Account(balance);
+	}
+
+	private void when_I_withdraw_$1(int amount) {
+		account.withdraw(amount);
+	}
+
+	private void then_my_balance_should_be_$1(int balance) {
+		assertEquals(balance, account.balance());
+	}
+}

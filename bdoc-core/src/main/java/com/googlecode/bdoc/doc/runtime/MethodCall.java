b085918c0085f138500d08f3cdd5c645251496cf
perottobc <perottobc@bf520d50-e954-0410-874e-2b0b529f9ca0>
2009-03-10 00:03:15 +0000

    experimental code analyzer, which runs the test and intercepts the method calls to retreive arguments to the methods

---
+++ bdoc-core/src/main/java/com/googlecode/bdoc/doc/runtime/MethodCall.java
@@ -0,0 +1,53 @@
+/**
+ * The MIT License
+ * 
+ * Copyright (c) 2008, 2009 @Author(s)
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package com.googlecode.bdoc.doc.runtime;
+
+import java.lang.reflect.Method;
+import java.lang.reflect.TypeVariable;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MethodCall {
+
+	private Method method;
+	private Object[] args;
+
+	public MethodCall(Method method, Object[] args) {
+		this.method = method;
+		this.args = args;
+	}
+
+	public String getName() {
+		return method.getName();
+	}
+
+	public List<Argument> getArguments() {
+		List<Argument> result = new ArrayList<Argument>();
+		
+		
+		return result;
+	}
+
+}
---
+++ bdoc-core/src/test/java/com/googlecode/bdoc/doc/runtime/testdata/AccountBehaviour.java
@@ -0,0 +1,72 @@
+/**
+ * The MIT License
+ * 
+ * Copyright (c) 2008, 2009 @Author(s)
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package com.googlecode.bdoc.doc.runtime.testdata;
+
+import org.junit.Test;
+
+/**
+ * Example should give the following doc:
+ * 
+ * <code>
+ * 
+ * Given my account has an initial balance of 200 
+ * When I ask to withdraw 20
+ * Then I should be given 20 and the new balance should be 180
+ * 
+ * </code>
+ * 
+ * @author Per Otto Bergum Christensen
+ * 
+ */
+public class AccountBehaviour {
+
+	@Test
+	public void withdraw() {
+		givenMyAccountHasAnInitialBalanceOf(200);
+		whenIAskToWithdraw(20);
+		thenIShouldBeGiven(20);
+		andTheNewBalanceShouldBe(180);
+	}
+
+	void givenMyAccountHasAnInitialBalanceOf(int initialBalance) {
+	}
+
+	void whenIAskToWithdraw(int amount) {
+	}
+
+	void thenIShouldBeGiven(int givenAmount) {
+	}
+
+	void andTheNewBalanceShouldBe(int newBalance) {
+	}
+
+	@Test
+	public void create() {
+		givenAnAccountNameAndInitalBalance("MyAccount", 200);
+	}
+
+	void givenAnAccountNameAndInitalBalance(String accountName, int initialBalance) {
+	}
+}

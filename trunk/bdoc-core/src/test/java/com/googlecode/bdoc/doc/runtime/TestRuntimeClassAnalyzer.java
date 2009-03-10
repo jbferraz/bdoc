c61527ef6011e7037dc0f2e8393e4318d21457e8
perottobc <perottobc@bf520d50-e954-0410-874e-2b0b529f9ca0>
2009-03-10 00:03:15 +0000

    experimental code analyzer, which runs the test and intercepts the method calls to retreive arguments to the methods

---
+++ trunk/bdoc-core/src/test/java/com/googlecode/bdoc/doc/runtime/TestRuntimeClassAnalyzer.java
@@ -0,0 +1,67 @@
+/**
+ * The MIT License
+ * 
+ * Copyright (c) 2008, 2009 @Author(s)
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package com.googlecode.bdoc.doc.runtime;
+
+import static org.junit.Assert.*;
+
+import java.util.List;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+import com.googlecode.bdoc.doc.runtime.testdata.AccountBehaviour;
+
+public class TestRuntimeClassAnalyzer {
+
+	private RuntimeClassAnalyzer runtimeClassAnalyzer = new RuntimeClassAnalyzer(AccountBehaviour.class);
+
+	@Test
+	public void shouldReturnAllMethodCallsInTheTestClassWhenRunningTheTest() {
+
+		List<MethodCall> methodCalls = runtimeClassAnalyzer.invoke("withdraw");
+
+		int index = 0;
+		assertEquals("givenMyAccountHasAnInitialBalanceOf", methodCalls.get(index++).getName());
+		assertEquals("whenIAskToWithdraw", methodCalls.get(index++).getName());
+		assertEquals("thenIShouldBeGiven", methodCalls.get(index++).getName());
+		assertEquals("andTheNewBalanceShouldBe", methodCalls.get(index++).getName());
+	}
+
+	@Test
+	@Ignore
+	public void shouldReturnNameOfMethodsArgumentsWithValue() {
+		MethodCall methodCall = runtimeClassAnalyzer.invoke("create").get(0);
+
+		assertEquals("givenAnAccountNameAndInitalBalance", methodCall.getName());
+
+		List<Argument> arguments = methodCall.getArguments();
+		assertEquals("accountName", arguments.get(0).getName());
+		assertEquals("MyAccount", arguments.get(0).getValue());
+
+		assertEquals("initialBalance", arguments.get(1).getName());
+		assertEquals(200, arguments.get(1).getValue());
+	}
+
+}

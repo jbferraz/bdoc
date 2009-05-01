c7d6ac6b5a622b82f674da56c4f19241ca132629
perottobc <perottobc@bf520d50-e954-0410-874e-2b0b529f9ca0>
2009-05-01 10:39:32 +0000

    pom oppdateringer, for at prosjektet skal kunne bygge med clean m2 repo

--- trunk/bdoc/bdoc-acceptance-test-support/src/main/java/com/googlecode/bdoc/testsupport/excel/ExcelTableCellReadException.java
+++ trunk/bdoc/bdoc-acceptance-test-support/src/main/java/com/googlecode/bdoc/testsupport/excel/ExcelTableCellReadException.java
@@ -1,24 +1,24 @@
 /**
- * The MIT License^M
- * ^M
- * Copyright (c) 2008, 2009 @Author(s)^M
- * ^M
- * Permission is hereby granted, free of charge, to any person obtaining a copy^M
- * of this software and associated documentation files (the "Software"), to deal^M
- * in the Software without restriction, including without limitation the rights^M
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell^M
- * copies of the Software, and to permit persons to whom the Software is^M
- * furnished to do so, subject to the following conditions:^M
- * ^M
- * The above copyright notice and this permission notice shall be included in^M
- * all copies or substantial portions of the Software.^M
- * ^M
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR^M
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,^M
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE^M
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER^M
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,^M
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN^M
+ * The MIT License
+ * 
+ * Copyright (c) 2008, 2009 @Author(s)
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
 
@@ -28,6 +28,7 @@ import org.apache.poi.hssf.usermodel.HSSFCell;
 
 public class ExcelTableCellReadException extends RuntimeException {
 
+	private static final long serialVersionUID = 1L;
 	private int rowIndex;
 	private int cellnum;
 	private HSSFCell cell;

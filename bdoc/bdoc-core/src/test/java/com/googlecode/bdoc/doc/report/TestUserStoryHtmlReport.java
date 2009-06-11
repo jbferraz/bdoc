57345c776b56318238c9f2141caf2a03cfcba723
micael.vesterlund <micael.vesterlund@bf520d50-e954-0410-874e-2b0b529f9ca0>
2009-06-11 20:22:30 +0000

    Formates the class name in the report.

--- bdoc/bdoc-core/src/test/java/com/googlecode/bdoc/doc/report/TestUserStoryHtmlReport.java
+++ bdoc/bdoc-core/src/test/java/com/googlecode/bdoc/doc/report/TestUserStoryHtmlReport.java
@@ -1,18 +1,18 @@
 /**
  * The MIT License
- * 
+ *
  * Copyright (c) 2008, 2009 @Author(s)
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -62,8 +62,7 @@ public class TestUserStoryHtmlReport {
 
 	public TestUserStoryHtmlReport() throws IOException {
 		bdoc = BDocTestHelper.bdocWithProject();
-		bdoc.addBehaviourFrom(new TestClass(TestClassWithThreeScenariosThreeSpecificationsAndThreeStatements.class),
-				BConst.SRC_TEST_JAVA);
+		bdoc.addBehaviourFrom(new TestClass(TestClassWithThreeScenariosThreeSpecificationsAndThreeStatements.class), BConst.SRC_TEST_JAVA);
 
 		UserStoryHtmlReport htmlReport = new UserStoryHtmlReport(bdoc);
 
@@ -107,8 +106,7 @@ public class TestUserStoryHtmlReport {
 
 	@Test
 	public void shouldPresentTheSpecificationsAssociatedWithTheStory() {
-		List<Specification> specifications = bdoc.getUserstories().get(0).getClassSpecifications().get(0)
-				.getSpecifications();
+		List<Specification> specifications = bdoc.getUserstories().get(0).getClassSpecifications().get(0).getSpecifications();
 		assertXPathContains(sentence(specifications.get(0)), "//ul[@class='specifications']", html);
 	}
 
@@ -119,9 +117,9 @@ public class TestUserStoryHtmlReport {
 	}
 
 	@Test
-	public void shouldPresentTheNameOfTheClassWithTheSpecifications() {
+	public void shouldPresentTheNameOfTheClassFormattedWithTheSpecifications() {
 		ClassSpecifications classBehaviour = bdoc.getUserstories().get(0).getClassSpecifications().get(0);
-		assertXPathContains(classBehaviour.getClassName(), "//ul[@class='specifications']", html);
+		assertXPathContains(new BDocMacroHelper().format(classBehaviour.getClassName()), "//ul[@class='specifications']", html);
 	}
 
 	@Test

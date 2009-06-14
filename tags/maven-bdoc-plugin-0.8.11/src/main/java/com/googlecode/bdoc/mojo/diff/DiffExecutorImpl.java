03f0e7875449af8d29ce133e4b40fb932ce4b730
perottobc <perottobc@bf520d50-e954-0410-874e-2b0b529f9ca0>
2009-06-14 12:30:43 +0000

    [maven-scm] copy for tag maven-bdoc-plugin-0.8.11

---
+++ tags/maven-bdoc-plugin-0.8.11/src/main/java/com/googlecode/bdoc/mojo/diff/DiffExecutorImpl.java
@@ -0,0 +1,57 @@
+/**
+ * The MIT License
+ * 
+ * Copyright (c) 2008, 2009 @Author(s)
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package com.googlecode.bdoc.mojo.diff;
+
+import java.io.File;
+
+import org.apache.commons.io.FileUtils;
+
+import com.googlecode.bdoc.diff.report.BDocDiffReport;
+import com.googlecode.bdoc.diff.report.DiffReport;
+
+/**
+ *  @author Per Otto Bergum Christensen
+ */
+public class DiffExecutorImpl implements DiffExecutor {
+
+	private File logDirectory;
+	
+	public DiffExecutorImpl(File logDirectory) {
+		super();
+		this.logDirectory = logDirectory;
+	}
+
+	public DiffReport createDiffReport(String oldBddDocFileName, String newBddDocFileName) {
+		try {
+
+			String oldXmlVersion = FileUtils.readFileToString(new File(logDirectory, oldBddDocFileName));
+			String newXmlVersion = FileUtils.readFileToString(new File(logDirectory, newBddDocFileName));
+
+			return new BDocDiffReport().execute(oldXmlVersion, newXmlVersion );
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+}

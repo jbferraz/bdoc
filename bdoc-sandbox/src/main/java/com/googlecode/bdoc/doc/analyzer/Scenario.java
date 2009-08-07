f06142cd803c36b900a5b8c9b07aed398da6c60a
micael.vesterlund <micael.vesterlund@bf520d50-e954-0410-874e-2b0b529f9ca0>
2009-08-07 21:16:54 +0000

    Some implementations.

---
+++ bdoc-sandbox/src/main/java/com/googlecode/bdoc/doc/analyzer/Scenario.java
@@ -0,0 +1,73 @@
+/**
+ * The MIT License
+ * 
+ * Copyright (c) 2008, 2009 @Author(s)
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package com.googlecode.bdoc.doc.analyzer;
+
+import java.util.Locale;
+
+import org.apache.commons.lang.Validate;
+
+/**
+ * @author Micael Vesterlund
+ */
+public class Scenario {
+
+	public static final int KEYWORD_COUNT = 3;
+
+	public enum Pattern {
+		NO(new Locale("no"), "og", "gitt", "Naar", "Saa"), EN(Locale.ENGLISH, "and", "given", "When", "Then"), SV(new Locale("sv"), "och",
+				"givet", "Naar", "Saa");
+
+		private final String and;
+		private final String[] keywords;
+		private final Locale locale;
+
+		private Pattern(Locale locale, String and, String... keywords) {
+			Validate.isTrue(KEYWORD_COUNT == keywords.length, "Scenario keywords length should be 3");
+			this.keywords = keywords;
+			this.locale = locale;
+			this.and = and;
+		}
+
+		public Locale locale() {
+			return locale;
+		}
+
+		public static boolean isScenario(String camelCaseSentence) {
+			for (Pattern pattern : Pattern.values()) {
+				for (String keyword : pattern.keywords) {
+					if (camelCaseSentence.startsWith(keyword.toLowerCase())) {
+						return true;
+					}
+				}
+			}
+			return false;
+		}
+
+		public String and() {
+			return and;
+		}
+	}
+
+}

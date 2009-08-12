6023d3f12facfa53a9dc3a0fa44dfda195237f6d
perottobc <perottobc@bf520d50-e954-0410-874e-2b0b529f9ca0>
2009-08-12 22:46:29 +0000

    pensjonsberegning, eksempelkode

--- bdoc-examples/pensjonsberegning/src/test/java/pensjonsberegning/TestFolketrygdberegningTjeneste.java
+++ bdoc-examples/pensjonsberegning/src/test/java/pensjonsberegning/TestFolketrygdberegningTjeneste.java
@@ -2,7 +2,6 @@ package pensjonsberegning;
 
 import static org.junit.Assert.assertEquals;
 
-import org.junit.Ignore;
 import org.junit.Test;
 
 import pensjonsberegning.bdoc.Ref;
@@ -10,16 +9,17 @@ import pensjonsberegning.bdoc.RefClass;
 import pensjonsberegning.bdoc.Story;
 
 @Ref(Story.BEREGNING_AV_ALDERSPENSJON)
-@RefClass( FolketrygdberegningTjeneste.class )
+@RefClass(FolketrygdberegningTjeneste.class)
 public class TestFolketrygdberegningTjeneste {
 
 	private GrunnbeloepTabell grunnbeloepTabell = new GrunnbeloepTabell();
 	private FolketrygdberegningTjeneste folketrygdberegningTjeneste = new FolketrygdberegningTjeneste(grunnbeloepTabell);
 
 	@Test
-	public void skalBeregneAlderspensjonForMedlemAvFolketrygden() {
+	public void skalBeregneAlderspensjonForEtMedlemAvFolketrygden() {
 
 		MedlemAvFolketrygden person = new MedlemAvFolketrygden();
+		// 40 år med maksimal opptjening
 		for (int aar = 1970; aar < 2010; aar++) {
 			person.addInntektForAar(aar, grunnbeloepTabell.gjennomsnittligGrunnbeloepFor(aar) * 12);
 		}
@@ -27,9 +27,11 @@ public class TestFolketrygdberegningTjeneste {
 		Alderspensjon alderspensjon = folketrygdberegningTjeneste.beregnAlderspensjonFor(person, 2009);
 		assertEquals(Grunnbeloep._2009.doubleValue(), alderspensjon.getGrunnpensjon(), .001);
 
-		assertEquals(8.33, alderspensjon.getTilleggspensjon().getSluttpoengtall(), .001);
-		
-//		assertEquals(0.44, alderspensjon.getTilleggspensjon().getPensjonsprosent(), .001);
-//		assertEquals(261367.2, alderspensjon.getTilleggspensjon().beregnet(), .1);
+		Tilleggspensjon tilleggspensjon = alderspensjon.getTilleggspensjon();
+
+		assertEquals(8.33, tilleggspensjon.getSluttpoengtall(), .001);
+		assertEquals(0.4365, tilleggspensjon.getPensjonsprosent(), .0001);
+		assertEquals(Grunnbeloep._2009, tilleggspensjon.getGrunnbeloep());
+		assertEquals(261817, tilleggspensjon.beregnet(), .1);
 	}
 }

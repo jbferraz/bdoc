91a8fc2b2b177a7e119bdd2244fe45c577c5b8f6
perottobc <perottobc@bf520d50-e954-0410-874e-2b0b529f9ca0>
2009-09-06 09:43:12 +0000

    [maven-scm] copy for tag maven-bdoc-plugin-0.8.14

--- bdoc-sandbox/src/test/java/com/googlecode/bdoc/doc/analyzer/ScenarioSupport.java
+++
@@ -1,9 +0,0 @@
-package com.googlecode.bdoc.doc.analyzer;
-
-@SuppressWarnings("unchecked")
-public class ScenarioSupport<T> {
-	T given = (T) this;
-	T and = (T) this;
-	T when = (T) this;
-	T then = (T) this;
-}

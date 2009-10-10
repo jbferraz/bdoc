abff4ef84652e30a851b1a3d883031d6f61e7f7a
perottobc <perottobc@bf520d50-e954-0410-874e-2b0b529f9ca0>
2009-10-10 21:45:35 +0000

    first cut on a yatzy-example

---
+++ trunk/bdoc-examples/yatzy/src/test/java/yatzy/Rule.java
@@ -0,0 +1,19 @@
+package yatzy;
+
+public enum Rule {
+
+	ONES, //
+	TWOS, //
+	THREES, //
+	FOURS, //
+	FIVES, //
+	SIXES, //
+	BONUS, //
+	THREE_OF_A_KIND, //
+	FOUR_OF_A_KIND, //
+	SMALL_STRAIGHT, //
+	LARGE_STRAIGHT, //
+	FULL_HOUSE, //
+	CHANCE, //
+	YATZY;
+}

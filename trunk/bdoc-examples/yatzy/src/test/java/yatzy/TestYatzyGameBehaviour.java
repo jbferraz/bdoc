2b5e4b97137b56b95131a4714141b199334cb012
perottobc <perottobc@bf520d50-e954-0410-874e-2b0b529f9ca0>
2009-10-12 21:51:58 +0000

    upgrade of yatzy example

--- trunk/bdoc-examples/yatzy/src/test/java/yatzy/TestYatzyGameBehaviour.java
+++ trunk/bdoc-examples/yatzy/src/test/java/yatzy/TestYatzyGameBehaviour.java
@@ -8,10 +8,8 @@ import java.util.List;
 import org.junit.Before;
 import org.junit.Test;
 
-import bdoc.Ref;
 import bdoc.RefClass;
 import bdoc.ScenarioSupport;
-import bdoc.Story;
 
 /**
  * Yatzy stuff:
@@ -33,29 +31,79 @@ public class TestYatzyGameBehaviour extends ScenarioSupport<TestYatzyGameBehavio
 		game = new Game();
 	}
 
+	// ----------------
+	@Test
+	public void shouldComputeScoreAccordingToChosenRule() {
+		example.singleRollComputation(Rule.ONES, asList(1, 1, 1, 1, 1), 5);
+		example.singleRollComputation(Rule.TWOS, asList(2, 2, 2, 2, 2), 10);
+		example.singleRollComputation(Rule.THREES, asList(3, 3, 3, 3, 3), 15);
+		example.singleRollComputation(Rule.FOURS, asList(4, 4, 4, 4, 4), 20);
+		example.singleRollComputation(Rule.FIVES, asList(5, 5, 5, 5, 5), 25);
+		example.singleRollComputation(Rule.SIXES, asList(6, 6, 6, 6, 6), 30);
+		example.singleRollComputation(Rule.THREE_OF_A_KIND, asList(1, 6, 6, 6, 1), 18);
+	}
+
+	void singleRollComputation(Rule rule, List<Integer> roll, int score) {
+		resetGame();
+		game.addRoll("unknown", roll, rule);
+		assertEquals("Computation with rule " + rule, score, game.score("unknown"));
+	}
+
+	// ----------------
 	@Test
 	public void shouldComputeScorePerPlayer() {
-		game.addRoll("perotto", asList(1, 2, 3, 4, 5));
-		game.addRoll("another", asList(1, 1, 1, 1, 1));
-		assertEquals(15, game.score("perotto"));
-		assertEquals(5, game.score("another"));
+		given.player(action("perotto", "has added", asList(1, 1, 1, 1, 1), Rule.ONES));
+		and.player(action("bob", "has added", asList(2, 2, 2, 2, 2), Rule.TWOS));
+		when.player(action("perotto", "adds", asList(6, 6, 6, 1, 1), Rule.THREE_OF_A_KIND));
+		then.ensureResult(new Result("perotto", 23));
+		and.ensureResult(new Result("bob", 10));
 	}
 
-	@Test
-	public void shouldComputeAllRollsForAGivenPlayer() {
-		game.addRoll("perotto", asList(1, 2, 3, 4, 5));
-		game.addRoll("perotto", asList(1, 1, 1, 1, 1));
-		assertEquals("Chance", 20, game.score("perotto"));
+	void player(Action score) {
+		game.addRoll(score.player, score.roll, score.rule);
 	}
 
-	@Test
-	public void shouldComputeTheScoreAccordingToRuleSpecified() {
-		example.rollComputation(Rule.ONES, asList(1, 1, 1, 1, 1), 5);
-		example.rollComputation(Rule.TWOS, asList(2, 2, 2, 1, 1), 6);
+	void scoreForPlayerShouldEqual(String player, int score) {
+		assertEquals(score, game.score(player));
 	}
 
-	void rollComputation(Rule rule, List<Integer> roll, int expectedRollScore) {
-
+	void ensureResult(Result score) {
+		assertEquals(score.score, game.score(score.player));
 	}
 
+	static Action action(String player, String msg, List<Integer> roll, Rule rule) {
+		return new Action(player, msg, roll, rule);
+	}
+
+	static class Action {
+		String player;
+		String msg;
+		Rule rule;
+		List<Integer> roll;
+
+		public Action(String player, String msg, List<Integer> roll, Rule rule) {
+			this.player = player;
+			this.msg = msg;
+			this.rule = rule;
+			this.roll = roll;
+		}
+
+		public String toString() {
+			return player + " " + msg + " " + rule.name().toLowerCase() + " " + roll + ", ";
+		}
+	}
+
+	static class Result {
+		String player;
+		int score;
+
+		public Result(String player, int score) {
+			this.player = player;
+			this.score = score;
+		}
+
+		public String toString() {
+			return ": " + player + " = " + score;
+		}
+	}
 }
